b32b549c5cb143dd0b78213564f11f67a34ebc31
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-12-15 13:03:44 -0500

    Templates html

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -20,7 +20,9 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home, name="home"),
     path('greet/', views.greet, name="greet"),
     path('farewell/', views.farewell, name="farewell"),
     path('adult/<int:age>/', views.adult, name="adult"),
+
 ]
--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html', {})
 
 
 def greet(request):
